6a6eff7ff7e6a890c57397f1c071e4c9871d7843
martinkro <martinkro@tencent.com>
2014-12-30 09:53:05 +0800

    init project

---
+++ disunity/disunity/DecryptAndUncompressFile.cs
@@ -0,0 +1,40 @@
+﻿using Ionic.Zlib;
+using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+
+public class BuildScript : MonoBehaviour {
+
+	void Start () {
+
+		FileStream objSourceEncryptFile =  new FileStream("~/dump.txt", 
+		                                                  FileMode.Open, 
+		                                                  FileAccess.ReadWrite);
+		FileStream objDestUnEncryptFile = new FileStream("~/DestDecryptFile.dll", 
+		                                                 FileMode.OpenOrCreate, 
+		                                                 FileAccess.ReadWrite);
+		string FileName_HashKey = "dump"; 
+		long FileSize = objSourceEncryptFile.Length;
+		byte [] ByReadData = new byte[FileSize];
+
+		objSourceEncryptFile.Read(ByReadData, 0, (Int32)FileSize); 
+		System.Random random = new System.Random(FileName_HashKey.GetHashCode());
+
+		for (int i = 0; i < ByReadData.Length; i++)
+		{
+			ByReadData[i] ^= (byte)random.Next(256);
+		}
+
+		byte[] Array =  ZlibStream.UncompressBuffer(ByReadData);
+
+		for (int i = 0; i < Array.Length; i++)
+		{
+			objDestUnEncryptFile.WriteByte(Array[i]);
+		}
+
+		
+		Debug.Log ("Write finished! ");
+	}
+}
---
+++ disunity/disunity/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using Ionic.Zlib;
+
+namespace disunity
+{
+	class MainClass
+	{
+		public static float ssdt(){
+			byte key = 188;
+			byte[] a = BitConverter.GetBytes( 140f );
+			for( int i = 0; i < a.Length; i++ )
+			{
+				a[ i ] ^= key;
+			}
+			float b = BitConverter.ToSingle( a, 0 );
+			byte[] c = BitConverter.GetBytes( b );
+			for( int i = 0; i < c.Length; i++ )
+			{
+				c[ i ] ^= key;
+			}
+			float d = BitConverter.ToSingle( c, 0 );
+			float r = d;
+			return r;
+		}
+		public static void Main (string[] args)
+		{
+			ssdt ();
+			/*
+			string name = "dump";
+			System.Random random = new System.Random (name.GetHashCode ());
+			for (int i = 0; i < 16; i++) {
+				byte key = (byte)random.Next (256);
+				Console.WriteLine ("key:" + key.ToString());
+			}
+			Console.WriteLine ();
+			*/
+			//test ();
+			Console.WriteLine ("Hello World!");
+		}
+
+		static void test(){
+			FileStream fs = File.OpenRead("dump.txt");
+			byte[] data = new byte[fs.Length];
+			Console.WriteLine("File Size:{0,0:d}", fs.Length);
+			fs.Read(data, 0, data.Length);
+			fs.Close();
+
+			for (int i = 0; i < 16; i++)
+			{
+				Console.Write("{0,2:x} ", data[i]);
+			}
+			Console.WriteLine();
+
+			string name = "dump";
+			Console.WriteLine("HashCode:{0,8:x}", name.GetHashCode());
+
+			System.Random random = new System.Random(name.GetHashCode());
+			for (int i = 0; i < data.Length; i++)
+			{
+				byte key = (byte)random.Next(256);
+				if (i < 16) {
+					Console.WriteLine("key:" + key.ToString());
+				}
+				data[i] ^= key;
+			}
+
+
+			for (int i = 0; i < 16; i++)
+			{
+				Console.Write("{0,2:x} ", data[i]);
+			}
+			Console.WriteLine();
+
+			byte[] data_decompressed = ZlibStream.UncompressBuffer(data);
+
+			fs = new FileStream("test.dll", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+			for (int i = 0; i < data_decompressed.Length; i++)
+			{
+			    fs.WriteByte(data_decompressed[i]);
+			}
+			fs.Close();
+		}
+	}
+}
